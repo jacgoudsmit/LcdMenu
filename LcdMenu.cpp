280daa66fdafd4c2ec7b64f5def9b4563c1f2019
Jac Goudsmit <jac_goudsmit@yahoo.com>
2013-09-29 00:53:44 -0700

    Adding LCD Menu library
    The LCD Menu library allows an Arduino sketch to generate a menu on an
    LCD screen, or in some other way when a full screen is not available.
    A menu can consist of an unlimited number of menu items, and the user
    can navigate through the items with a previous/next/select interface.
    When an item is selected, a function pointer can be called. Sub-menus
    can be implemented by using the menu_Enter and menu_Leave functions.
    All menu items are stored in flash memory to minimize the RAM footprint.
    Only the current item and the current menu are copied to RAM when
    necessary. For now, it's not possible to generate menus on the fly or
    modify them at runtime (though some magic can be done with the
    Navigation function, see below).
    The library doesn't call any LCD function explicitly; instead it calls a
    function that needs to be provided by the application, which should show
    the current menu title or the current menu item in some appropriate way.
    For 2-line LCD's, the program should probably show the current menu
    title on the top line and the current item on the bottom line. The
    entire state of the library is public (via external variables) to make
    it easy for a sketch to implement a menu in its own way.
    A Navigation function can be defined per menu which is called whenever
    the user navigates through the menu (enters a menu, moves the selection
    to a different item or selects the item).

---
+++ LcdMenu.cpp
@@ -0,0 +1,182 @@
+/*
+  Menu generator
+  --------------
+  By Jac Goudsmit
+  Distributed under the BSD (3-clause) license.
+*/
+
+
+/////////////////////////////////////////////////////////////////////////////
+// INCLUDES
+/////////////////////////////////////////////////////////////////////////////
+
+
+#include <Arduino.h>
+
+#define MENU_EXT
+#include "LcdMenu.h"
+
+
+/////////////////////////////////////////////////////////////////////////////
+// CODE
+/////////////////////////////////////////////////////////////////////////////
+
+
+//---------------------------------------------------------------------------
+//! Initialize the current menu item and show it on the second line
+static boolean
+load_item()
+{
+  boolean result = false;
+
+  memcpy_P(&menu_gCurItem, &menu_gCurMenu.m_items[menu_gpCurState->m_itemindex], 
+    sizeof(menu_gCurItem));
+  if (menu_gCurItem.m_name)
+  {
+    menu_ShowCB(MENUSHOW_ITEM);
+    result = true;
+  }
+
+  return result;
+}
+
+
+//---------------------------------------------------------------------------
+//! Initialize the menu and show it on the top line
+//
+// This doesn't initialize the current item.
+static void
+load_menu()
+{
+  memcpy_P(&menu_gCurMenu, menu_gpCurState->m_pmenu, sizeof(menu_gCurMenu));
+  menu_ShowCB(MENUSHOW_MENU);
+}
+
+
+//---------------------------------------------------------------------------
+//! Enter a given menu, increasing the stack depth. Show menu and first item
+MENUITEMFUNC_PROTO(menu_EnterMenu)
+{
+  Menu PROGMEM *menu = (Menu PROGMEM *)menuparm;
+
+  // Go to next entry of stack
+  ++menu_gpCurState;
+
+  menu_gpCurState->m_pmenu = menu;
+  load_menu();
+
+  menu_gpCurState->m_itemindex = 0;
+
+  // If the new menu has a navigation function, call it immediately
+  // The navigation function may change the current item so don't load the
+  // initial item until afterwards.
+  if (menu_gCurMenu.m_navfunc)
+  {
+    (void)menu_gCurMenu.m_navfunc(MENUNAV_MENU);
+  }
+
+  load_item();
+}
+
+
+//---------------------------------------------------------------------------
+//! Leave the current menu. Show the previous menu and its most recent item
+MENUITEMFUNC_PROTO(menu_LeaveMenu)
+{
+  if (menu_gpCurState != &menu_gStack[0])
+  {
+    --menu_gpCurState;
+  }
+
+  load_menu();
+  load_item();
+}
+
+
+//---------------------------------------------------------------------------
+//! Initialize menu
+void menu_Init(
+  Menu *pmenu)
+{
+  menu_gpCurState = &menu_gStack[-1]; // Following line will change index to 0
+  menu_EnterMenu(pmenu);
+}
+
+
+//---------------------------------------------------------------------------
+//! Process menu action
+void
+menu_Process(
+  MenuAction action)
+{
+  switch(action)
+  {
+  case MENUACTION_PREV:
+    {
+      if (menu_gpCurState->m_itemindex > 0)
+      {
+        menu_gpCurState->m_itemindex--;
+        load_item();
+
+        // If there's a navigation function, call it when selection changes
+        if (menu_gCurMenu.m_navfunc)
+        {
+          (void)menu_gCurMenu.m_navfunc(MENUNAV_CHANGE);
+        }
+      }
+    }
+    break;
+
+  case MENUACTION_NEXT:
+    {
+      menu_gpCurState->m_itemindex++;
+
+      if (!load_item())
+      {
+        menu_gpCurState->m_itemindex--;
+      }
+      else
+      {
+        // If there's a navigation function, call it when selection changes
+        if (menu_gCurMenu.m_navfunc)
+        {
+          (void)menu_gCurMenu.m_navfunc(MENUNAV_CHANGE);
+        }
+      }
+    }
+    break;
+
+  case MENUACTION_SELECT:
+    {
+      // If there's a navigation function, pushing Select leaves the menu
+      // after calling the navigation function in "leave" mode.
+      // But we should get the navigation function here (not after calling
+      // the item function) because the item function might enter a different
+      // menu which would make us get the wrong navigation function from the
+      // wrong menu.
+      MenuNavFunc_t *navfunc = menu_gCurMenu.m_navfunc;
+
+      if (menu_gCurItem.m_func)
+      {
+        menu_gCurItem.m_func(menu_gCurItem.m_parm);
+      }
+
+      if (navfunc)
+      {
+        if (navfunc(MENUNAV_SELECT))
+        {
+          menu_LeaveMenu(NULL);
+        }
+      }
+    }
+    break;
+
+  default:
+    ; // Nothing to do here
+  }
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// END
+/////////////////////////////////////////////////////////////////////////////
---
+++ LcdMenu.h
@@ -0,0 +1,178 @@
+/*
+  Menu generator
+  --------------
+  By Jac Goudsmit
+  Distributed under the BSD (3-clause) license.
+*/
+
+
+#ifndef LCDMENU_H
+#define LCDMENU_H
+
+
+/////////////////////////////////////////////////////////////////////////////
+// MACROS
+/////////////////////////////////////////////////////////////////////////////
+
+
+#define MENU_STACKDEPTH (10)
+
+#ifndef MENU_EXT
+#define MENU_EXT extern
+#endif
+
+
+/////////////////////////////////////////////////////////////////////////////
+// TYPES
+/////////////////////////////////////////////////////////////////////////////
+
+
+//---------------------------------------------------------------------------
+// Parameter passed to display callback function
+enum MenuShow
+{
+  MENUSHOW_MENU,
+  MENUSHOW_ITEM,
+};
+
+
+//---------------------------------------------------------------------------
+// Parameter passed to processing function
+enum MenuAction
+{
+  MENUACTION_NONE,                      // No action
+  MENUACTION_PREV,                      // Switch to previous menu item
+  MENUACTION_NEXT,                      // Switch to next menu item
+  MENUACTION_SELECT,                    // Select the current item
+};
+
+
+//---------------------------------------------------------------------------
+// Parameter passed to navigation function
+enum MenuNav
+{
+  MENUNAV_MENU,                         // Entering a menu
+  MENUNAV_CHANGE,                       // Current menu item has changed
+  MENUNAV_SELECT,                       // User pressed Select button
+};
+
+
+//---------------------------------------------------------------------------
+// Function type for menu items
+//
+// A function of this type can be called whenever a menu option is selected.
+#define MENUITEMFUNC_PROTO(name) void name(void *menuparm)
+typedef MENUITEMFUNC_PROTO(MenuItemFunc_t);
+
+
+//---------------------------------------------------------------------------
+// Function type for navigation function
+//
+// A function of this type can be connected to each menu. It is called at
+// the following occasions:
+// - Just after the menu is loaded that the navigation function is attached
+//   to. The function can change the current item before returning, e.g. to
+//   show the current setting.
+// - After the user changes the selection with the Up or Down button (but
+//   only if the selection was actually changed, not when the user tries to
+//   navigate past the first or last options)
+// - After the user makes a selection, and the according menu item function
+//   has been called. The navigation function can return true to leave the
+//   menu in this case (the return values for the other cases is ignored).
+//   Note, if a menu item function was called before calling the navigation
+//   function, the current menu may have changed and you may not want to
+//   return true or you will leave the wrong menu!
+#define MENUNAVFUNC_PROTO(name) boolean name(MenuNav navmode)
+typedef MENUNAVFUNC_PROTO(MenuNavFunc_t);
+
+
+//---------------------------------------------------------------------------
+// Type definition for a menu item
+struct MenuItem
+{
+  prog_char        *m_name;             // EEPROM based string, NULL=end list
+  MenuItemFunc_t   *m_func;             // Optional item function
+  void             *m_parm;             // Parameter to pass to function
+};
+#define MENUITEMLIST(name) MenuItem name[] PROGMEM = {
+#define MENUITEMLISTEND { NULL, NULL, NULL } };
+#define MENUITEM(name, func, parm) { name, func, (void*)parm },
+
+
+//---------------------------------------------------------------------------
+// Type definition for a menu
+struct Menu
+{
+  prog_char        *m_menuname;         // EEPROM based string for screen
+  MenuItem PROGMEM *m_items;            // EEPROM based list of items
+  MenuNavFunc_t    *m_navfunc;          // Optional navigation function
+};
+
+
+//---------------------------------------------------------------------------
+// Menu navigation stack item
+struct MenuState
+{
+  Menu PROGMEM     *m_pmenu;            // Pointer to menu
+  byte              m_itemindex;        // Selected item
+};
+
+
+/////////////////////////////////////////////////////////////////////////////
+// DATA
+/////////////////////////////////////////////////////////////////////////////
+
+
+//---------------------------------------------------------------------------
+// Working variables
+MENU_EXT Menu       menu_gCurMenu;      // RAM copy of current menu
+MENU_EXT MenuItem   menu_gCurItem;      // RAM copy of current item
+MENU_EXT MenuState  menu_gStack[MENU_STACKDEPTH]; // Stack of selected items
+MENU_EXT MenuState *menu_gpCurState;    // Pointer to current state on stack
+
+
+/////////////////////////////////////////////////////////////////////////////
+// FORWARD DECLARATIONS
+/////////////////////////////////////////////////////////////////////////////
+
+
+//---------------------------------------------------------------------------
+// Forward declarations for menu item functions
+MenuItemFunc_t 
+  menu_EnterMenu,                       // Enter menu (parm=pointer to Menu)
+  menu_LeaveMenu;                       // Leave current menu (parm ignored)
+
+
+/////////////////////////////////////////////////////////////////////////////
+// PUBLIC FUNCTIONS
+/////////////////////////////////////////////////////////////////////////////
+
+
+//---------------------------------------------------------------------------
+//! Initialize menu
+void menu_Init(
+  Menu *pmenu);
+
+
+//---------------------------------------------------------------------------
+//! Process menu action
+void 
+menu_Process(
+  MenuAction action);
+
+
+//---------------------------------------------------------------------------
+//! Show something on the output device
+//
+// This is provided by the application and called by the menu module
+void
+menu_ShowCB(
+  MenuShow displaymode);
+
+
+/////////////////////////////////////////////////////////////////////////////
+// END
+/////////////////////////////////////////////////////////////////////////////
+
+
+#endif
